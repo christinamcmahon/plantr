2c3950a54761cf5f23eaedd4457f4620e8cb592f
christinamcmahon <christina.mcmahon1@gmail.com>
2020-03-20 14:22:24 -0700

    add plant info to users

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,22 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :username, :avatar_url, :email, :notification
+  attributes :id, :name, :username, :avatar_url, :email, :notification, :plant_info
+
+  def plant_info
+    if self.object.plants
+      {
+        plants: self.object.plants.map { |plant|
+          # byebug
+          {
+            plant: {
+              id: plant.id,
+              name: plant.name,
+              notes: plant.notes,
+              water_frequency: plant.water_frequency,
+              image_url: plant.image_url,
+            },
+          }
+        },
+      }
+    end
+  end
 end
